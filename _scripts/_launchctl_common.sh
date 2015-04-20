d5dcc75a7a8f8ceadabe4b41c12c9c5d412d77c9
Viktor Benei <viktor.benei@gmail.com>
2015-04-20 14:36:45 +0200

    launchctl script fix

--- _scripts/_launchctl_common.sh
+++ _scripts/_launchctl_common.sh
@@ -3,6 +3,6 @@
 server_label_id="bitrise.io.tools.cmd-bridge"
 curr_user_lib_launch_agents_dir="$HOME/Library/LaunchAgents"
 server_plist_path="${curr_user_lib_launch_agents_dir}/${server_label_id}.plist"
-server_full_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd)/bin/osx/xcuserver"
+server_full_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd)/bin/osx/cmd-server"
 server_logs_dir_path="${HOME}/logs"
 server_log_file_path="${server_logs_dir_path}/${server_label_id}.log"
